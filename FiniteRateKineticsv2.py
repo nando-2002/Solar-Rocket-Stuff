e7b0f3538c4805ea4981c130418284fa4d038fd7
unknown <prthik.kar@gmail.com>
2024-10-14 18:51:53 +0530

    Isentropic solution finished, kinetics solution to be completed

--- FiniteRateKineticsv2.py
+++ FiniteRateKineticsv2.py
@@ -15,6 +15,7 @@ def areaRatio (val):
     
     return area_ratio_squared**0.5
 
+#from Mach num, P, T and v can be calculated with isentropic relations
 def machnumbers ():
    
     subsonicM = np.linspace(0.3, 1, 1000)
@@ -49,7 +50,6 @@ def machnumbers ():
     
     return actualM
     
-#from Mach num, P, T and v can be calculated with isentropic relations
 
 #creating the nozzle profile from Krieger 1951
 
@@ -68,4 +68,11 @@ gam = 1.36
 
 M = machnumbers()
 
-plt.plot(x[:], M[:])
+#defining some initial conditions
+
+Po, To, P, T = 20, 3500, np.zeros(x.size), np.zeros(x.size)
+P = Po * (1 + (gam - 1)/2 * M**2)**((-gam)/(gam - 1))
+T = To * (1 + (gam - 1)/2 * M**2)**(-1)
+#plt.plot(x[:], T[:])
+
+#from here, the chemical kinetic solution can begin
